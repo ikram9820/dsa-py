0b409c046bb0c9aecbadcf3065646d1fee5fa7c5
ikram9820 <ikram9820khan@gmail.com>
2023-05-10 16:25:46 +0500

    implementing queue with two stacks

--- Queue.py
+++ Queue.py
@@ -3,15 +3,28 @@ from Stack import Stack
 class Queue:
     def __init__(self):
         self.items = []
+        self.stack1 = Stack()
+        self.stack2 = Stack()
 
     def enqueue(self, item):
         self.items.insert(0,item)
+        self.stack1.push(item)
     
     def dequeue(self):
-        return self.items.pop()
+        # return self.items.pop()
+        self.stack1ToStack2()
+        return self.stack2.pop()
+    
+    def stack1ToStack2(self):
+        if(self.stack2.isEmpty()):
+            while not self.stack1.isEmpty():
+                self.stack2.push(self.stack1.pop())
+
 
     def front(self):
-        return self.items[-1]
+        # return self.items[-1]
+        self.stack1ToStack2()
+        return self.stack2.peek()
     
     def isEmpty(self):
         return self.items==[]
@@ -37,6 +50,5 @@ if __name__ == "__main__":
     q.enqueue(6)
     q.enqueue(9)
     # q.dequeue()
-    print(q)    
-    q.reverse()
-    print(q)
+    print(q.front())    
+    # q.reverse()
